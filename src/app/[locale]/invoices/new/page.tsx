7d769b9d82fd62be1512670f1eac31706fc51d92
Jonathan Pillajo <vh-ventura@outlook.com>
2026-05-10 19:32:04 -0500

    style: remove max-w constraint on invoice form so it fills available width

--- src/app/[locale]/invoices/new/page.tsx
+++ src/app/[locale]/invoices/new/page.tsx
@@ -37,7 +37,7 @@ export default async function NewInvoicePage({
   const catalogs: InvoiceCatalogs = { idTypes, paymentMethods, taxRates };
 
   return (
-    <div className="max-w-2xl">
+    <div>
       <PageHeader title={t('title')} />
       <InvoiceForm catalogs={catalogs} />
     </div>
